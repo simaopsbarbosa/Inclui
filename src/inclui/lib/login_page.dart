d1a4207f955e261f0116e60f6a771d8c9b79c292
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-04-09 23:53:24 +0100

    fixed login screen and profile page navigation

--- src/inclui/lib/login_page.dart
+++ src/inclui/lib/login_page.dart
@@ -13,17 +13,19 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
   String? _errorMessage;
 
-  void _updateState() {
-    setState(() {});
-  }
-
   Future<void> _signIn() async {
     try {
       await _auth.signInWithEmailAndPassword(
-        email: _emailController.text,
+        email: _emailController.text.trim(),
         password: _passwordController.text,
       );
-      _updateState();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Login successful'),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+      );
+      Navigator.pop(context, true);
     } catch (e) {
       setState(() {
         _errorMessage = _formatFirebaseError(e);
@@ -34,10 +36,16 @@ class _LoginPageState extends State<LoginPage> {
   Future<void> _register() async {
     try {
       await _auth.createUserWithEmailAndPassword(
-        email: _emailController.text,
+        email: _emailController.text.trim(),
         password: _passwordController.text,
       );
-      _updateState();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Registration successful'),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+      );
+      Navigator.pop(context, true);
     } catch (e) {
       setState(() {
         _errorMessage = _formatFirebaseError(e);
@@ -45,18 +53,10 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  Future<void> _signOut() async {
-    await _auth.signOut();
-    _updateState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    User? user = _auth.currentUser;
-
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.white),
         title: Text(
           'Authentication',
           style: GoogleFonts.inter(
@@ -66,6 +66,7 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
         backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.white),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(32.0),
@@ -80,133 +81,91 @@ class _LoginPageState extends State<LoginPage> {
                 height: 64,
               ),
             ),
-            Center(
-              child: user != null
-                  ? Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Logged in as: ',
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.inter(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Text(
-                          '${user.email}',
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.inter(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        SizedBox(height: 16),
-                        ElevatedButton(
-                          onPressed: _signOut,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Theme.of(context).primaryColor,
-                          ),
-                          child: Text(
-                            'Logout',
-                            style: GoogleFonts.inter(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    )
-                  : Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextField(
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white,
-                            hintText: 'Email',
-                            hintStyle: GoogleFonts.inter(
-                                fontSize: 14, color: Colors.grey[700]),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: BorderSide.none,
-                            ),
-                            contentPadding: EdgeInsets.symmetric(
-                                horizontal: 12, vertical: 14),
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        SizedBox(height: 12),
-                        TextField(
-                          controller: _passwordController,
-                          style: GoogleFonts.inter(
-                              fontSize: 14, color: Colors.white),
-                          decoration: InputDecoration(
-                            filled: false,
-                            hintText: 'Password',
-                            hintStyle: GoogleFonts.inter(
-                                fontSize: 14, color: Colors.grey[700]),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: BorderSide(
-                                color: Colors.grey.shade900,
-                                width: 1.5,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: BorderSide(
-                                color: Theme.of(context).primaryColor,
-                                width: 2,
-                              ),
-                            ),
-                            contentPadding: EdgeInsets.symmetric(
-                                horizontal: 12, vertical: 14),
-                          ),
-                          obscureText: true,
-                        ),
-                        SizedBox(height: 16),
-                        if (_errorMessage != null)
-                          Text(
-                            _errorMessage!,
-                            style: GoogleFonts.inter(
-                                fontSize: 12, color: Colors.red),
-                          ),
-                        SizedBox(height: 16),
-                        Column(
-                          children: [
-                            ElevatedButton(
-                              onPressed: _signIn,
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      Theme.of(context).primaryColor,
-                                  minimumSize: Size(120, 40)),
-                              child: Text(
-                                'Login',
-                                style: GoogleFonts.inter(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.white),
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: _register,
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white,
-                                  minimumSize: Size(120, 40)),
-                              child: Text(
-                                'Register',
-                                style: GoogleFonts.inter(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600,
-                                    color: Theme.of(context).primaryColor),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                hintText: 'Email',
+                hintStyle: GoogleFonts.inter(
+                  fontSize: 14,
+                  color: Colors.grey[700],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 12),
+            TextField(
+              controller: _passwordController,
+              style: GoogleFonts.inter(fontSize: 14, color: Colors.white),
+              decoration: InputDecoration(
+                filled: false,
+                hintText: 'Password',
+                hintStyle:
+                    GoogleFonts.inter(fontSize: 14, color: Colors.grey[700]),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade900,
+                    width: 1.5,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).primaryColor,
+                    width: 2,
+                  ),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 16),
+            if (_errorMessage != null)
+              Text(
+                _errorMessage!,
+                style: GoogleFonts.inter(fontSize: 12, color: Colors.red),
+              ),
+            SizedBox(height: 16),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: _signIn,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    minimumSize: Size(120, 40),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: GoogleFonts.inter(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _register,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    minimumSize: Size(120, 40),
+                  ),
+                  child: Text(
+                    'Register',
+                    style: GoogleFonts.inter(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Theme.of(context).primaryColor),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
--- src/inclui/lib/main.dart
+++ src/inclui/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:geolocator/geolocator.dart';
 import 'firebase_options.dart';
 import 'login_page.dart';
@@ -33,48 +34,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// Determine the current position of the device.
-///
-/// When the location services are not enabled or permissions
-/// are denied the `Future` will return an error.
 Future<Position> _determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
-    // Location services are not enabled don't continue
-    // accessing the position and request users of the
-    // App to enable the location services.
     return Future.error('Location services are disabled.');
   }
-
-  permission = await Geolocator.checkPermission();
+  LocationPermission permission = await Geolocator.checkPermission();
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
       return Future.error('Location permissions are denied');
     }
   }
-
   if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately.
     return Future.error(
         'Location permissions are permanently denied, we cannot request permissions.');
   }
-
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
   return await Geolocator.getCurrentPosition();
 }
 
 class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -103,6 +83,25 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Future<void> _handleAuthButton() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        _selectedIndex = 3;
+      });
+    } else {
+      final result = await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+      if (result == true) {
+        setState(() {
+          _selectedIndex = 3;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,14 +119,9 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
+              onPressed: _handleAuthButton,
               child: Text(
-                'Login',
+                FirebaseAuth.instance.currentUser != null ? 'Profile' : 'Login',
                 style: GoogleFonts.inter(
                   color: Theme.of(context).primaryColor,
                   fontSize: 16,
@@ -245,7 +239,6 @@ class _SearchPageState extends State<SearchPage> {
             newReports.add(value['timestamp']);
           }
         });
-
         setState(() {
           _reports = newReports.reversed.toList();
         });
@@ -272,72 +265,70 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blueAccent,
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Reports',
-                    style: GoogleFonts.inter(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w700,
-                    ),
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Reports',
+                  style: GoogleFonts.inter(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-                if (_reports.isNotEmpty)
-                  IconButton(
-                    icon: Icon(
-                      Icons.delete_forever,
-                      color: Colors.red,
-                      size: 32,
-                    ),
-                    onPressed: _clearReports,
+              ),
+              if (_reports.isNotEmpty)
+                IconButton(
+                  icon: Icon(
+                    Icons.delete_forever,
+                    color: Colors.red,
+                    size: 32,
                   ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              child: _reports.isEmpty
-                  ? Center(
-                      child: Text(
-                        'No reports found',
-                        style: GoogleFonts.inter(
-                          fontSize: 18,
-                        ),
+                  onPressed: _clearReports,
+                ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: _reports.isEmpty
+                ? Center(
+                    child: Text(
+                      'No reports found',
+                      style: GoogleFonts.inter(
+                        fontSize: 18,
                       ),
-                    )
-                  : ListView.builder(
-                      itemCount: _reports.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          color: Colors.grey.shade100,
-                          margin: EdgeInsets.symmetric(vertical: 5),
-                          child: ListTile(
-                            leading: Icon(Icons.report, color: Colors.black),
-                            title: Text(
-                              'Report #${index + 1}',
-                              style: GoogleFonts.inter(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            subtitle: Text(
-                              _reports[index],
-                              style: GoogleFonts.inter(fontSize: 14),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: _reports.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        color: Colors.grey.shade100,
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        child: ListTile(
+                          leading: Icon(Icons.report, color: Colors.black),
+                          title: Text(
+                            'Report #${index + 1}',
+                            style: GoogleFonts.inter(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
                             ),
                           ),
-                        );
-                      },
-                    ),
-            ),
-          ],
-        ),
+                          subtitle: Text(
+                            _reports[index],
+                            style: GoogleFonts.inter(fontSize: 14),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
       ),
     );
   }
@@ -381,4 +372,3 @@ class ReportPage extends StatelessWidget {
     );
   }
 }
-
