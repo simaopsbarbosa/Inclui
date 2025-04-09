b1ec81b2e9799799719ba4a2abd4930aa0839e5c
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-04-10 00:13:58 +0100

    fixed profile page so that it updates automatically and redirects user to login page if not logged in

--- src/inclui/lib/profile_page.dart
+++ src/inclui/lib/profile_page.dart
@@ -1,11 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'login_page.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  late User? _user;
+  late final Stream<User?> _authStateChanges;
 
-  void _signOut(BuildContext context) async {
+  @override
+  void initState() {
+    super.initState();
+    _user = _auth.currentUser;
+    _authStateChanges = _auth.authStateChanges();
+    _authStateChanges.listen((user) {
+      setState(() {
+        _user = user;
+      });
+    });
+  }
+
+  void _signOut() async {
     await _auth.signOut();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
@@ -14,44 +34,77 @@ class ProfilePage extends StatelessWidget {
         duration: Duration(seconds: 2),
       ),
     );
-    Navigator.popUntil(context, (route) => route.isFirst);
+  }
+
+  void _redirectToLogin() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final user = _auth.currentUser;
-
     return Container(
       color: Colors.deepPurpleAccent,
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (user != null)
-              Text(
-                'Welcome, ${user.email}',
-                style: GoogleFonts.inter(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                ),
+        child: _user != null
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome, ${_user!.email}',
+                    style: GoogleFonts.inter(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _signOut,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                    ),
+                    child: Text(
+                      'Logout',
+                      style: GoogleFonts.inter(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'You are not logged in. \nLog in to access exclusive features.',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _redirectToLogin,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                    ),
+                    child: Text(
+                      'Login',
+                      style: GoogleFonts.inter(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _signOut(context),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
-              ),
-              child: Text(
-                'Logout',
-                style: GoogleFonts.inter(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
