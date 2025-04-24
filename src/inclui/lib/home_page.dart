13495864f43a5d89645b62c44578ee85cf5b10d1
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-04-24 21:40:49 +0100

    added (broken) google maps integration

---
+++ src/inclui/lib/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+  return await Geolocator.getCurrentPosition();
+}
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Position>(
+      future: _determinePosition(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            color: Colors.lightGreenAccent,
+            child: Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
+                backgroundColor: Colors.black,
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          Position userLocation = snapshot.data!;
+          return GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(userLocation.latitude, userLocation.longitude),
+              zoom: 14.0,
+            ),
+          );
+        } else {
+          return Center(child: Text('No location data available'));
+        }
+      },
+    );
+  }
+}
